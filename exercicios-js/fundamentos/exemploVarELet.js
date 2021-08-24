c1419f35a3d1d19c590d6b5a02d4084c45e33303
Gilson Aureliano <gilson.aureliano22@gmail.com>
2021-08-24 11:53:47 -0300

    exemplo

---
+++ exercicios-js/fundamentos/exemploVarELet.js
@@ -0,0 +1,26 @@
+const funcs = []
+
+for (var i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i)
+        
+    })
+}
+
+funcs[2]() //10
+funcs[8]() //10
+
+
+console.log('---------------------------------------')
+
+const funcs1 = []
+
+for (let i = 0; i < 10; i++){
+    funcs1.push(function(){
+        console.log(i)
+        
+    })
+}
+
+funcs1[2]() //2
+funcs1[8]() //8
